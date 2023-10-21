d5c88f50d3707b048e8cc0bf2d5db662c2506fc2
elmiramm <elmira.m.m@yandex.ru>
2023-10-21 16:04:44 +0300

    header

--- src/js/modules/toggleBurger.js
+++ src/js/modules/toggleBurger.js
@@ -1,7 +1,8 @@
 export function toggleBurger() {
 	const pageHeader = document.querySelector('.page-header');
 	const burger = document.querySelector('.burger');
-	const menu = document.querySelector('.page-header__right');
+	const menuMain = document.querySelector('.page-header__main-nav');
+	const menuUser = document.querySelector('.page-header__user-nav');
 	if (pageHeader) {
 		pageHeader.addEventListener('click', (e) => {
 			const target = e.target;
@@ -9,11 +10,13 @@ export function toggleBurger() {
 				if (burger.getAttribute('aria-label') === "Open menu") {
 					burger.classList.add('active-burger');
 					burger.setAttribute('aria-label', 'Close menu');
-					menu.classList.add('page-header__right--open');
+					menuMain.classList.add('page-header__main-nav--open');
+					menuUser.classList.add('page-header__user-nav--open');
 				} else {
 					burger.classList.remove('active-burger');
 					burger.setAttribute('aria-label', 'Open menu');
-					menu.classList.remove('page-header__right--open');
+					menuMain.classList.remove('page-header__main-nav--open');
+					menuUser.classList.remove('page-header__user-nav--open');
 				}
 			}
 		})
