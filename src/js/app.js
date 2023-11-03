af8571c127be29a76b62ef4cab26c170653a7cba
elmiramm <elmira.m.m@yandex.ru>
2023-11-03 19:45:37 +0300

    things - html, js, swiper

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import { isWebp } from "./modules/iswebp.js";
 import { swiperSlider } from "./modules/swiperslider.js";
-import { toggleBurger } from "./modules/toggleBurger.js";
+import { toggleBurger } from "./modules/toggle-burger.js";
 import { tabs } from "./modules/tabs.js";
 
 
--- src/js/modules/swiperslider.js
+++ src/js/modules/swiperslider.js
@@ -26,13 +26,59 @@ export function swiperSlider() {
 			// autoplay: {
 			// 	delay: 3000, //Пауза м.д. прокрутками
 			// 	stopOnLastSlide: false,
-			// 	disabledOnInteraction: false,
+			// 	disableOnInteraction: false,
 			// },
 			effect: 'slide', //значение по умолчанию - перелистывает слайды, 'fade' - смена прозрачности
 			// navigation: {
 			// 	nextEl: '.slider-section .slider-arrow--next',
 			// 	prevEl: '.slider-section .slider-arrow--prev',
 			// },
+			// breakpoints: { //Mobile First - значения от заданной точки и больше 
+			// 	279: {
+			// 		slidesPerView: 1.1,
+			// 		spaceBetween: 15,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 20,
+			// 	},
+			// 	992: {
+			// 		slidesPerView: 3,
+			// 		spaceBetween: 32,
+			// 	},
+			// }
+		});
+	}
+	if (document.querySelector('.slider-things__body')) {
+		new Swiper('.slider-things__body', {
+			observer: true, //обновляет swiper при изменении элементов слайдера
+			observeParents: true, //обновляет swiper при изменении в родительских элементах слайдера
+			slidesPerView: 4.5,
+			spaceBetween: 0,
+			slidesPerGroup: 1, //если нужно чтобы кол-во dots было меньше кол-ва slides. Slides будут перелистываться группами
+			// centeredSlides: true, //Активный слайд по центру
+			loop: true, // за последним слайдем будет идти заново первый
+			watchOverflow: false,
+			speed: 800,
+			loopAdditionalSlides: 3,
+			preloadImage: false,
+			// parallax: true,
+			simulateTouch: true, //вкл/выкл перетаскинавия на ПК
+			// pagination: {
+			// 	el: '.slider-section__controls .dots',
+			// 	clickable: true, //true - можно кликнуть на dotts и перейти на нужный слайд
+			// 	dynamicBullets: false, // ]true - dots будут менять размер в зависимости от того насколько далеко расположены от активного dots
+			// },
+			autoplay: {
+				delay: 1000, //Пауза м.д. прокрутками
+				stopOnLastSlide: false,
+				disableOnInteraction: false,
+			},
+			// effect: 'slide', //значение по умолчанию - перелистывает слайды, 'fade' - смена прозрачности
+			navigation: {
+				nextEl: '.slider-things__controls .slider-arrow--next',
+				prevEl: '.slider-things__controls .slider-arrow--prev',
+			},
 			// breakpoints: { //Mobile First - значения от заданной точки и больше 
 			// 	279: {
 			// 		slidesPerView: 1.1,
